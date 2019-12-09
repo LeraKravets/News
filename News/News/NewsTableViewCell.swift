ec984205754b066e52ba577310528d84c0ecb771
Lera <kravetsvaleryia@gmail.com>
2019-12-09 15:27:53 +0300

    added: NewsTableView, class for Cell, outlets for image and labels

---
+++ News/News/NewsTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  NewsTableViewCell.swift
+//  News
+//
+//  Created by Лера on 12/9/19.
+//  Copyright © 2019 com.vkravets. All rights reserved.
+//
+
+import UIKit
+
+class NewsTableViewCell: UITableViewCell {
+
+    // MARK: - Outlets
+    @IBOutlet weak var newsSmallImage: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var dateLbl: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- News/News/ViewController.swift
+++ News/News/ViewController.swift
@@ -10,11 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - Outlets
+
+    @IBOutlet weak fileprivate var newsTableView: UITableView!
+
+    // MARK: - Properties
+
+    private let newsCellID = "NewsCellID"
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.newsTableView.dataSource = self
+        self.newsTableView.delegate = self
+    }
+
+
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 2
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: newsCellID, for: indexPath) as? NewsTableViewCell else {
+            fatalError("Can't find cell with id: \(newsCellID)")
+        }
+        return cell
     }
 
 
 }
 
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 80.0
+    }
+}
