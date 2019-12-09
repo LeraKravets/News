a277099bac549331872ce31bae7726db9c54eb6c
Lera <kravetsvaleryia@gmail.com>
2019-12-09 14:35:45 +0300

    created: project

---
+++ News/News/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  News
+//
+//  Created by Лера on 12/9/19.
+//  Copyright © 2019 com.vkravets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
